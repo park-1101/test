daa9f9d70c95ee0000099ef4fce8c2dd185fbc7f
park-1101 <gammaflicker@gmail.com>
2025-03-24 10:31:11 +0900

    Add files via upload

---
+++ audio-processor.js
@@ -0,0 +1,38 @@
+class AudioProcessor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.port.onmessage = (e) => {
+      const { type, data } = e.data;
+      if (type === 'toggleRecording') this.isRecording = data.isRecording;
+      if (type === 'setThreshold') this.threshold = data.threshold;
+    };
+    this.isRecording = false;
+    this.threshold = 22;
+  }
+
+  process(inputs, outputs, parameters) {
+    const leftData = inputs[0][0] || new Float32Array(128);
+    const rightData = inputs[0][1] || new Float32Array(128);
+    let leftMaxDb = -Infinity;
+    let rightMaxDb = -Infinity;
+
+    for (let i = 0; i < leftData.length; i++) {
+      const leftSample = leftData[i] * 32768;
+      const rightSample = rightData[i] * 32768;
+      const leftDb = leftSample === 0 ? -Infinity : 20 * Math.log10(Math.abs(leftSample));
+      const rightDb = rightSample === 0 ? -Infinity : 20 * Math.log10(Math.abs(rightSample));
+      if (leftDb > leftMaxDb) leftMaxDb = leftDb;
+      if (rightDb > rightMaxDb) rightMaxDb = rightDb;
+    }
+
+    const maxDb = Math.max(leftMaxDb, rightMaxDb);
+    this.port.postMessage({
+      type: 'audioData',
+      data: { leftMaxDb, rightMaxDb, maxDb, timestamp: currentTime * 1000 } // 밀리초로 변환
+    });
+
+    return true;
+  }
+}
+
+registerProcessor('audio-processor', AudioProcessor);
